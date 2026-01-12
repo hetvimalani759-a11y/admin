04455a1e0a585be619a9003b2f7e3dd76c6e54b3
hetvimalani759-a11y <hetvimalani759@gmail.com>
2026-01-12 16:39:07 +0530

    change in models

--- optiview/adminpanel/models.py
+++ optiview/adminpanel/models.py
@@ -55,6 +55,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Notification(models.Model):
+    title=models.CharField(max_length=20)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     message = models.TextField()
     is_read = models.BooleanField(default=False)
