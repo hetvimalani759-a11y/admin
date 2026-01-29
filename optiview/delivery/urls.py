a26928ff2ef0cad3353dc738d273f241ca17e265
harsha1227 <harshanavadiya074@gmail.com>
2026-01-29 18:10:19 +0530

    edit_profile of delivery person

--- optiview/delivery/urls.py
+++ optiview/delivery/urls.py
@@ -12,6 +12,8 @@ urlpatterns = [
     path('dashboard/', views.delivery_dashboard, name='delivery_dashboard'),
     path('forgot-password/', views.forgot_password, name='forgot_password'),
     path('reset-password/<uidb64>/<token>/', views.reset_password, name='reset_password'),
-    path('logout/', views.delivery_logout, name='logout'),
+    path('logout/', views.delivery_logout, name='delivery_logout'),
     path("my-orders/", views.my_orders, name="my_orders"),
+    path('profile/', views.delivery_profile, name='delivery_profile'),
+     path('profile/edit/', views.edit_profile, name='edit_profile'),
 ]
